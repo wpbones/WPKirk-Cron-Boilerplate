3d9bac63a8c2e7cfce7b4016800065d08ac3e1c5
Giovambattista Fazioli <gfazioli@users.noreply.github.com>
2024-10-29 11:05:49 +0100

    Initial commit

---
+++ config/menus.php
@@ -0,0 +1,58 @@
+<?php
+
+if (!defined('ABSPATH')) {
+  exit();
+}
+
+/*
+|--------------------------------------------------------------------------
+| Plugin Menus routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all the menu routes for a plugin.
+| In this context, the route are the menu link.
+|
+*/
+
+return [
+  'wp_kirk_slug_menu' => [
+    "page_title" => "WP Kirk Page",
+    "menu_title" => "WP Kirk Menu",
+    'capability' => 'read',
+    'icon' => 'wpbones-logo-menu.png',
+    'items' => [
+      [
+        "page_title" => "DB",
+        "menu_title" => "DB",
+        'capability' => 'read',
+        'route' => [
+          'get' => 'Examples\DatabaseController@db'
+        ],
+      ],
+      [
+        "page_title" => "Model",
+        "menu_title" => "Model",
+        'capability' => 'read',
+        'route' => [
+          'get' => 'Examples\DatabaseController@simpleModel'
+        ],
+      ],
+      [
+        "page_title" => "Model NoPrefix",
+        "menu_title" => "Model NoPrefix",
+        'capability' => 'read',
+        'route' => [
+          'get' => 'Examples\DatabaseController@noPrefixModel'
+        ],
+      ],
+      [
+        "page_title" => "Eloquent ORM",
+        "menu_title" => "Eloquent ORM",
+        'capability' => 'read',
+        'route' => [
+          'get' => 'Examples\DatabaseController@eloquent'
+        ],
+      ],
+    ]
+  ]
+];
